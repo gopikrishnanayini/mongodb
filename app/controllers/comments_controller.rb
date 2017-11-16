7f1b4785ea05ff65df4997511a37056c1ac5d0f4
sushiltech123 <nayinigopi93@gmail.com>
2017-11-16 13:26:11 +0530

    associations data added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,31 +2,32 @@ class CommentsController < ApplicationController
   before_action :load_post
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
-  # GET /comments
-  # GET /comments.json
+
   def index
+    @post = Post.find(params[:post_id])
     @comments = @post.comments
   end
 
-  # GET /comments/1
-  # GET /comments/1.json
+
   def show
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
   end
 
-  # GET /comments/new
   def new
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.build
   end
 
-  # GET /comments/1/edit
   def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
   end
 
-  # POST /comments
-  # POST /comments.json
-  def create
-    @comment = @post.comments.build(comment_params)
 
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
     respond_to do |format|
       if @comment.save
         format.html { redirect_to [@post, @comment], notice: 'Comment was successfully created.' }
@@ -38,9 +39,9 @@ class CommentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
   def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
     respond_to do |format|
       if @comment.update_attributes(comment_params)
         format.html { redirect_to [@post, @comment], notice: 'Comment was successfully updated.' }
@@ -52,9 +53,9 @@ class CommentsController < ApplicationController
     end
   end
 
-  # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
     @comment.destroy
     respond_to do |format|
       format.html { redirect_to post_comments_url(@post) }
@@ -63,12 +64,11 @@ class CommentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_comment
       @comment = @post.comments.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
       params.require(:comment).permit(:body)
     end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,28 +1,26 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  # GET /posts
-  # GET /posts.json
+
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
+
   def show
+    @post = Post.find(params[:id])
   end
 
-  # GET /posts/new
+
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
+    @post = Post.find(params[:id])
   end
 
-  # POST /posts
-  # POST /posts.json
+
   def create
     @post = Post.new(post_params)
 
@@ -37,8 +35,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
   def update
     @post = Post.find(params[:id])
     respond_to do |format|
@@ -52,9 +48,6 @@ class PostsController < ApplicationController
     end
   end
 
-
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
@@ -65,12 +58,10 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
     end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
+    
     def post_params
       params.require(:post).permit(:title, :body, :starred)
     end
